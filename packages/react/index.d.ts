998cc267176f9d034fd0af65d0c2c8e1f92d25fc
shiro <shiro@usagi.io>
2018-09-06 08:14:08 +0200

    Fix mixThemeWithProps TS definition return type

--- packages/react/index.d.ts
+++ packages/react/index.d.ts
@@ -31,18 +31,19 @@ export type Theme<T = {}> = Partial<T> & {
 
 /**
  * @param ownTheme  First CSS modules object, used as a default (origin) theme for composition
- * @param props     Standard react props object with following properties:
+ * @param props     Standard react props object containing theming properties
  * @param options   Additional options
  *
  * @return theme
  */
-export function getThemeFromProps<T = {}>(ownTheme: object, props: ThemeProps<T>, options?: ThemeOptions): Theme<T>;
+export function getThemeFromProps<T>(ownTheme: Theme<T>, props: ThemeProps, options?: ThemeOptions): Theme<T>;
 
 /**
- * @param ownTheme First CSS modules object, used as a default (origin) theme for composition
- * @param props    Standard react props object with following properties:
- * @param options  Additional options
+ * @param ownTheme             First CSS modules object, used as a default (origin) theme for composition
+ * @param props                Standard react props object with following properties:
+ * @param options              Additional options
  *
- * @return theme
+ * @return props & boxed theme
  */
-export function mixThemeWithProps<T = {}>(ownTheme: object, props: ThemeProps<T>, options?: ThemeOptions): Theme<T>;
+export function mixThemeWithProps<T, U extends ThemeProps>(ownTheme: Theme<T>, props: U, options?: ThemeOptions):
+    { theme: Theme<T> } & Pick<U, Exclude<keyof U, keyof ThemeProps>>
