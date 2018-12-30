45e7b40a8f484e86fb6f68329bc9969c6f9e758c
klimashkin <klimashkin@gmail.com>
2018-12-30 00:58:59 -0800

    Add speddtest.js that can be run as 'node -r esm ./packages/core/speedtest.js'

---
+++ packages/core/speedtest.js
@@ -0,0 +1,35 @@
+/**
+ * Run this using 'node -r esm ./packages/core/speedtest.js'
+ */
+import {composeTheme} from './dist/core.es2018';
+
+const themeIcon = {
+  icon: 'Icon_icon',
+  small: 'Icon_small Icon_icon',
+  medium: 'Icon_medium Icon_icon',
+  svg: 'Icon_svg',
+};
+const themeButton = {
+  button: 'Button_button',
+  primary: 'Button_primary Button_button',
+  secondary: 'Button_secondary Button_button',
+  text: 'Button_text',
+  color: 'Button_color',
+  size: 'Button_size',
+  'success-icon': 'Button_success-icon Button_color',
+  'success-svg': 'Button_success-svg Button_size',
+};
+let start;
+let i;
+
+for (start = Date.now(), i = 0; i < 1e6; i++) {
+  composeTheme([{theme: themeIcon, noCache: true}, {theme: themeButton, prefix: 'success-'}]);
+}
+
+console.log(`1 million compositions no Cache   have been done in ${Date.now() - start}ms`);
+
+for (start = Date.now(), i = 0; i < 1e6; i++) {
+  composeTheme([{theme: themeIcon}, {theme: themeButton, prefix: 'success-'}]);
+}
+
+console.log(`1 million compositions with Cache have been done in ${Date.now() - start}ms`);
